c26c3609dec3318ee43b6572b5425fd9b8b41c2d
Naveen Prajapati <prajapatinaveen7003@gmail.com>
2024-10-12 00:35:12 +0530

    fetch data from api

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,11 @@
+import Link from 'next/link'
 import React from 'react'
 
 const page = () => {
   return (
-    <div className="text-3xl">Welcome to homepage</div>
+    <div className="text-3xl"><h1>Welcome to homepage</h1>
+    <Link href="/fetchapi">Go to api page</Link></div>
+    
   )
 }
 
